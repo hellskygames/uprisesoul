12ce7a3e46fba1cecd4eb88c7ed08d71d340cc47
Estudiante <francastel2902@gmail.com>
2022-11-12 16:05:09 -0500

    plataforms

---
+++ PathFollow2DScript.gd
@@ -0,0 +1,15 @@
+extends PathFollow2D
+
+
+onready var animationPlayer = $AnimationPlayer
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	animationPlayer.play("horizontal")
+	animationPlayer.play("moving")
+pass
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
