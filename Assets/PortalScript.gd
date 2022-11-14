9f8fb0e93dccfcd177acb1fe809a15f4079abc94
unknown <francastel2902@gmail.com>
2022-11-13 20:33:32 -0500

    puertas,portales y mapas

--- Assets/PortalScript.gd
+++ Assets/PortalScript.gd
@@ -5,7 +5,7 @@ export (String) var escena
 
 func _on_Portal_body_entered(body):
 	if body.name == "Player":
-		if Input.is_action_just_pressed("ui_focus_next"):
+		if Input.is_action_just_pressed("ui_left"):
 			get_tree().change_scene("res://Escenas/Levels/Debug_levels/"+escena+".tscn")
 	pass # Replace with function body.
 	
