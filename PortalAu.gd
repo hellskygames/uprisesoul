c820e905eaadd272b4e798ed60f413d45f05097f
unknown <francastel2902@gmail.com>
2022-11-14 23:24:24 -0500

    Portales, jugador2.0, mapas, errores

--- PortalAu.gd
+++ PortalAu.gd
@@ -4,7 +4,7 @@ export (String) var escena
 
 func _on_PortalAu_body_entered(bodyy):
 	if bodyy.name == "Player":
-			get_tree().change_scene("res://Escenas/Levels/Debug_levels/"+escena+".tscn")
+			get_tree().change_scene("res://Escenas/Levels/Debug_levels/nivel3.tscn")
 	pass # Replace with function body.
 
 
