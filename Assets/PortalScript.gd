26b4685a983d9ffc22ba0af6c71d178ca48a7480
leon <juan7leon4@gmail.com>
2022-11-18 17:51:23 -0600

    Se agregar 4 enemigos

--- Assets/PortalScript.gd
+++ Assets/PortalScript.gd
@@ -5,8 +5,7 @@ export (String) var escena
 
 func _on_Portal_body_entered(body):
 
-	if Input.is_action_pressed("ui_focus_next"):
-		if body.name == "Player":
-			get_tree().change_scene("res://Escenas/Levels/Debug_levels/Nivel2.tscn")
+	if body.name == "Player":
+		get_tree().change_scene("res://Escenas/Levels/Debug_levels/"+escena+".tscn")
 	pass # Replace with function body.
 	
